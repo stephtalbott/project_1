5385e7645c41945a891c5e39425528ccaff90206
stephtalbott <talbottstephanie@ymail.com>
2023-04-26 20:01:29 -0500

    both game and user sequences are functional

--- js/main.js
+++ js/main.js
@@ -3,20 +3,14 @@ const COLORS = ["yellow", "red", "green", "blue"];
 
 /*----- state variables -----*/
 
-let gameSequence = []
-let playerSequence = []
-let level = 0
-let maxTurns = 10
-let turn
-let winner 
-let box
-let color
+let gameSequence;
+let playerSequence;
+let level;
+
 /*----- cached elements  -----*/
 const startBtn = document.getElementById("start-btn");
 const playAgainBtn = document.getElementById("play-again-btn");
 const info = document.querySelector(".info");
-const roundCounter = document.getElementById("round-counter");
-
 
 const boxes = {
   yellow: document.getElementById("yellow"),
@@ -27,7 +21,7 @@ const boxes = {
 
 /*----- event listeners -----*/
 startBtn.addEventListener("click", startGame);
-// playAgainBtn.addEventListener("click", )
+playAgainBtn.addEventListener("click", startGame);
 /*----- functions -----*/
 
 init();
@@ -37,137 +31,109 @@ function init() {
   playAgainBtn.classList.add("hidden");
   info.classList.remove("hidden");
   info.textContent = "press start to begin!";
-  turn = 1;
+  level = 0;
+  playerSequence = [];
+  gameSequence = [];
 }
 
 // this uses the random sequence to start the first round
 function startGame() {
-    startBtn.classList.add("hidden");
-    info.classList.remove("hidden");
-    info.textContent = "wait for the computer";
-    randomSequence();
-    playRound(0);
+  init();
+  startBtn.classList.add("hidden");
+  info.classList.remove("hidden");
+  info.textContent = "wait for the computer";
+  randomSequence();
+  //   playRound(0);
 }
-
-// this increments the level var and pushes the random sequence into the empty gameSequence array 
-// function nextRound() {
-//     level += 1; 
-//     const nextInSequence = Array.from(gameSequence); 
-//     nextInSequence.push(randomSequence()); 
-// }
 
 // this generates a 10-value random sequence to be displayed by the computer
 function randomSequence() {
-//   for (let i = 0; i < 10; i++) {
-  let randomIndex = Math.floor(Math.random() * 4);
-  gameSequence.push(COLORS[randomIndex]);
-  //}
-    let i = 0;
-    let intervalId = setInterval(function () {
-      //   console.log(gameSequence[i]);
-      i++;
-      if (i >= gameSequence.length) {
-        clearInterval(intervalId);
-      }
-    // if (i === playerSequence.length) {
-    //   clearInterval(intervalId);
-    // }
-    }, 1000);
-  console.log("this is game Seq in randomSequence()", gameSequence);
-}
+  let oldPlayerSequence = playerSequence;
+  playerSequence = [];
+  level++;
+  document.getElementById("level").innerHTML = level;
+  let randomBox = randomBoxFunc();
+  gameSequence.push(randomBox);
 
+  // play old records first
+  //playOld(oldPlayerSequence);
 
-// this passes in the current index of the gameSequence to flash/play sound of the appropriate square 
-function playRound(currentIndex) {
-    const color = gameSequence[currentIndex];
-    const box = boxes[color];
+  playSquare(oldPlayerSequence, 0);
+
+  setTimeout(() => {
+    let box = document.querySelector(`#${randomBox}`);
     box.classList.add("active");
     const auId = box.getAttribute("data-box");
-    playAudio(auId);
+    playAudio(auId); // play new record
     setTimeout(() => {
-    console.log('this is currentIndex', currentIndex)
+      box.classList.remove("active");
+      info.textContent = "your turn";
+    }, 500);
+  }, gameSequence.length * 500);
+}
+
+function playSquare(oldPlayerSequence, i) {
+  if (i === oldPlayerSequence.length) return;
+
+  let el = oldPlayerSequence[i];
+  let box = document.querySelector(`#${el}`);
+  box.classList.add("active");
+  playAudio("audio_" + el); // e.g audio_red | audio_yellow ...
+  info.textContent = "watch for the pattern";
+  setTimeout(() => {
     box.classList.remove("active");
-    if (currentIndex === playerSequence.length) {
-      console.log("correct");
-    } else {
-      setTimeout(playRound, 1500, (currentIndex += 1));
-    }
-  }, 1000);
-  userClick();
+  }, 500);
+
+  setTimeout(() => {
+    return playSquare(oldPlayerSequence, i + 1);
+  }, 500);
 }
 
-
-
-// this listens for a user click to flash/play the appropriate audio 
-// and stores each click in the playerSeq array but dang this is long and hard to manipulate :( 
-// function userClick() {
-//   const squares = document.querySelectorAll(".square");
-//   squares.forEach((box) => {
-//     box.addEventListener("click", (e) => {
-//       const auId = box.getAttribute("data-box");
-//       playAudio(auId);
-//       playerSequence.push(e.target.id);
-//       turn += 1;
-//       console.log("playerSeq", playerSequence);
-//       console.log("gameSeq", gameSequence);
-//       if (gameSequence.length === playerSequence.length) {
-//         randomSequence();
-//       }
-//       const clickedSquare = e.target.id;
-//     //   console.log("this is clickedSquare", clickedSquare);
-//       let currentIndex = 0;
-//       const correctColor = gameSequence[currentIndex];
-//     //   console.log("this is gameSequence[currentIndex]", gameSequence[currentIndex]);
-//       if (clickedSquare === correctColor) {
-//         playRound(currentIndex);
-//       } else {
-//         console.log("failed");
-//         currentIndex = 0;
-//       }
-//     });
-//   });
-// }
-
-// tried to break it into smaller functions 
-function userClick() {
-  const squares = document.querySelectorAll(".square");
-  squares.forEach((box) => {
-    box.addEventListener("click", (e) => {
-      handleUserClick(box, e);
-    });
+const squares = document.querySelectorAll(".square");
+squares.forEach((box) => {
+  box.addEventListener("click", (e) => {
+    let userChosenSequence = box.id; // red | yellow | blue ...
+    playerSequence.push(userChosenSequence);
+    const auId = box.getAttribute("data-box");
+    playAudio(auId);
+    playGame(playerSequence.length - 1);
   });
-}
+});
 
-function handleUserClick(box, e) {
-  const auId = box.getAttribute("data-box");
-  playAudio(auId);
-  playerSequence.push(e.target.id);
-  turn += 1;
-  console.log("playerSeq", playerSequence);
-  console.log("gameSeq", gameSequence);
-  if (gameSequence.length === playerSequence.length) {
-    randomSequence();
-  }
-  checkUserClick(e.target.id);
-}
-
-function checkUserClick(clickedSquare) {
-  let currentIndex = 0;
-  const correctColor = gameSequence[currentIndex];
-  if (clickedSquare === correctColor) {
-    playRound(currentIndex);
+function playGame(currentLevel) {
+  if (gameSequence[currentLevel] === playerSequence[currentLevel]) {
+    if (playerSequence.length === gameSequence.length) {
+      setTimeout(() => {
+        randomSequence();
+      }, 1000);
+    }
   } else {
-    console.log("failed");
-    currentIndex = 0;
+    gameOver();
   }
 }
 
-// this makes the sounds work for both comp and player 
+function gameOver() {
+  level = 0;
+  playerSequence = [];
+  info.textContent = "you lost! try again";
+  playAgainBtn.classList.remove("hidden");
+}
+
+// this makes the sounds work for both comp and player
 function playAudio(id) {
   const audio = document.getElementById(id);
   audio.play();
 }
 
+function randomBoxFunc() {
+  let randomIndex = Math.floor(Math.random() * 4);
+  let randomBox = COLORS[randomIndex]; // red | yellow | blue ...
+  if (gameSequence[gameSequence.length - 1] === randomBox) {
+    randomBox = randomBoxFunc();
+  }
+  return randomBox;
+}
+
 // to work on next:
-// tracking the users clicks and putting them in an array 
-// making sure the game increments based on guessing a round correctly 
+// tracking the users clicks and putting them in an array
+// making sure the game increments based on guessing a round correctly
